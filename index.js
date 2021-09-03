8de3def05e2580d963718ee9a222c728d0d0810a
Daniel Williams <danielpaulwill@gmail.com>
2021-09-03 17:03:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+
+function createEmployeeRecord(array) {
+  let employeeRecord = {
+    firstName: `${array[0]}`,
+    familyName: `${array[1]}`,
+    title: `${array[2]}`,
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return employeeRecord
+};
+
+function createEmployeeRecords(employeeRecords) {
+  console.log(`employeeRecords length: ${employeeRecords.length}`)
+  employeeRecords.map(employee => {
+    console.log(`employee length: ${employee.length}`)
+    createEmployeeRecord(employee)
+    }
+  )};
+
+  let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+  ]
+
+  createEmployeeRecords(twoRows)
