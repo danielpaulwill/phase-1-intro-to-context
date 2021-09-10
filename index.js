e9b82262c037978f24dedcf3a5482acdddfd8609
Daniel Williams <danielpaulwill@gmail.com>
2021-09-10 14:11:14 -0400

    commit tree c3a91264985345df27b4ca0bd874a7ee6a0be455

--- index.js
+++ index.js
@@ -39,12 +39,25 @@ function createTimeOutEvent(employeeRecord, dateStamp) {
   return employeeRecord
 }
 
+function hoursWorkedOnDate(employeeRecord, date) {
+  let hourIn = employeeRecord.timeInEvents.find(timeIn => date === timeIn.date)
+  let hourOut = employeeRecord.timeOutEvents.find(timeOut => date === timeOut.date)
+  let hoursWorked = ((hourOut.hour - hourIn.hour)/100)
+  return hoursWorked
+  };
+
+function wagesEarnedOnDate(employeeRecord, date) {
+let payOwed = hoursWorkedOnDate(employeeRecord, date) * employeeRecord.payPerHour
+return payOwed
+};
 
 
 
 
 
-let employeeRecord = {
+
+const employeeRecordArray = [
+employeeRecord1 = {
   firstName: 'Garfield',
   familyName: 'Arbuckle',
   title: 'Cat',
@@ -69,78 +82,54 @@ let employeeRecord = {
     hour: 1500,
     date: "2014-03-28"
   }]
+},
+employeeRecord2 = {
+  firstName: 'Odie',
+  familyName: 'Arbuckle',
+  title: 'Dog',
+  payPerHour: 5,
+  timeInEvents: [{
+    type: "TimeIn",
+    hour: 1100,
+    date: "2014-02-28"
+  },
+  {
+    type: "TimeIn",
+    hour: 1000,
+    date: "2014-03-28"
+  }],
+  timeOutEvents: [{
+    type: "TimeOut",
+    hour: 1400,
+    date: "2014-02-28"
+  },
+  {
+    type: "TimeOut",
+    hour: 1500,
+    date: "2014-03-28"
+  }]
+}
+];
+
+function allWagesFor(employeeRecord) {
+  let wagesArray = []
+  let employeeEvents = employeeRecord.timeInEvents
+  employeeEvents.map(timeInDays => {
+    let wages = wagesEarnedOnDate(employeeRecord, timeInDays.date)
+    wagesArray.push(wages)
+  })
+  let reducer = (previousValue, currentValue) => previousValue + currentValue;
+  let allWages = wagesArray.reduce(reducer)
+  return allWages
 };
 
-// FUNCTION BEFORE MESSING WITH IT
-// function hoursWorkedOnDate(employeeRecord, date) {
-//   console.log(`date before if: ${date}`)
-//   if (date === employeeRecord.timeInEvents[0].date) {
-// let hoursWorked = (employeeRecord.timeOutEvents[0].hour - employeeRecord.timeInEvents[0].hour)/100
-// console.log(`hoursWorked: ${hoursWorked}`)
-// return hoursWorked
-// } else console.log('No match found')
-// };
-
-// FUNCTION CORRECTLY ITERATES THROUGH timeInEvents, NOW NEED TO MATCH THAT WITH timeOutEvents
-// function hoursWorkedOnDate(employeeRecord, date) {
-//   employeeRecord.timeInEvents.map(object => {
-//     if (date === object.date){
-//       console.log(object)
-//       let hoursWorked = (employeeRecord.timeOutEvents[0].hour - object.hour)/100
-//       console.log(`hoursWorked: ${hoursWorked}`)
-//     }}
-//   )};
-
-
-
-
-function hoursWorkedOnDate(employeeRecord, date) {
-  let hoursWorked = employeeRecord.timeInEvents.map(timeInObject => {
-    if (date === timeInObject.date){
-    let inOutHours1 = employeeRecord.timeOutEvents.map(timeOutObject => {
-        if (timeInObject.date === timeOutObject.date){
-          let inOutHours2 = (timeOutObject.hour - timeInObject.hour)/100
-          console.log(`inOutHours2: ${inOutHours2}`)
-          console.log(`inOutHours2 type: ${typeof inOutHours2}`)
-          return inOutHours2
-        }
-      })
-      console.log(`inOutHours1: ${inOutHours1}`)
-      console.log(`inOutHours1 type: ${typeof inOutHours1}`)
-      return inOutHours1
-    }
+function calculatePayroll(array) {
+  let allEmployeesWages = []
+  array.map(employee => {
+    let employeeWages = allWagesFor(employee)
+    allEmployeesWages.push(employeeWages)
   })
-    console.log(`hoursWorked[0][0]: ${hoursWorked[0][0]}`)
-    console.log(`hoursWorked type: ${typeof hoursWorked}`)
-    return hoursWorked
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-// function wagesEarnedOnDate(employeeRecord, date) {
-// let payOwed = hoursWorkedOnDate(employeeRecord, date) * employeeRecord.payPerHour
-// console.log(`payOwed inside of wagesEarnedOnDate: ${payOwed}`)
-// return payOwed
-// };
-
-// function allWagesFor(employeeRecord) {
-//   let allWages = []
-//   console.log(`allWages before map: ${allWages}`)
-//   let employeeEvents = employeeRecord.timeInEvents
-//   console.log(employeeEvents)
-//   employeeEvents.map(timeInDays => {
-//     let wages = wagesEarnedOnDate(employeeRecord, timeInDays.date)
-//     console.log(`wages after wagesEarnedOnDate: ${wages}`) 
-//     allWages.push(wages)
-//   })
-//   console.log(`allWages after map: ${allWages}`)
-// };
+  let reducer = (previousValue, currentValue) => previousValue + currentValue;
+  let allWages = allEmployeesWages.reduce(reducer)
+  return allWages
+}
